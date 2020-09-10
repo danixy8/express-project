65f8ba1594ec075901aaf6bd770a7eabb6097717
Daniel <Danielguve2@gmail.com>
2020-09-09 22:04:35 -0300

    segundo commit dia miercoles 9/9

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ const express = require('express')
 const app = express();
 
 const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
 //express hbs engine
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
+
 app.get('/', (req, res) => {
 
     res.render('home', {
-        nombre: 'Daniel',
-        anio: new Date().getFullYear()
+        nombre: 'daniel'
     });
 
 });
 
 app.get('/about', (req, res) => {
 
-    res.render('about', {
-        anio: new Date().getFullYear()
-    })
+    res.render('about')
 })
 
 // app.get('/', (req, res) => {
@@ -42,6 +43,6 @@ app.get('/data', (req, res) => {
     
     });
  
-app.listen(3000, ()=>{
-    console.log('Escuchando en el puerto 3000');
+app.listen(port, ()=>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 })
